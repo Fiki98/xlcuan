7b7ec30da55b698b8349de018babadd3c5f8f3b6
Fiki98 <108824485+Fiki98@users.noreply.github.com>
2025-10-15 18:17:54 +0700

    Update hot.py

--- app/menus/hot.py
+++ app/menus/hot.py
@@ -17,7 +17,7 @@ def show_hot_menu():
     while in_bookmark_menu:
         clear_screen()
         print_header("🔥 Paket Hot Pertama 🔥")
-        url = "https://me.mashu.lol/pg-hot.json"
+        url = "https://pastebin.com/raw/kH0nKg9Z"
         response = requests.get(url, timeout=30)
         if response.status_code != 200:
             print("Gagal mengambil data hot package.")
@@ -86,7 +86,7 @@ def show_hot_menu2():
     while in_bookmark_menu:
         clear_screen()
         print_header("🔥 Paket Hot Kedua 🔥")
-        url = "https://me.mashu.lol/pg-hot2.json"
+        url = "https://pastebin.com/raw/xSSrLBJj"
         response = requests.get(url, timeout=30)
         if response.status_code != 200:
             print("Gagal mengambil data hot package.")
